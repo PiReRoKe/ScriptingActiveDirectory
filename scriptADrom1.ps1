9757baec969fce6aed81a5532ebbaa0fe836d87a
knonn <romain.besland@gmail.com>
2015-11-20 14:20:04 +0100

    create_users & lire_fichier functions added

--- scriptADrom1.ps1
+++ scriptADrom1.ps1
@@ -1,1 +1,43 @@
-$local_user=[ASDI]"WinNT://."
+$fichier="C:\users.txt"
+$logfile="C:\logfile.log"
+
+function create_users {
+    param([string[]]$params)
+    $local=[ADSI]"WinNT://."
+    $nom=$params[0]
+    $nom_complet=$params[1]
+    $description=$params[2]
+    $compte=[ADSI]"WinNT://./$nom"
+    $date=Get-Date -UFormat "%Y-%m-%d %H:%M"
+    If (!$compte.path)
+    {
+        $utilisateur=$local.create("user",$nom)
+        $utilisateur.InvokeSet("Description",$description)
+        $utilisateur.InvokeSet("FullName",$nom_complet)
+        $utilisateur.CommitChanges()
+        Write "Add $nom : success" >> $logfile
+    }
+    else {
+        Write "[$date] User $nom already exist" >> $logfile
+    }
+}
+
+function lire_fichier {
+    param([string]$file_path)
+    If (Test-Path $file_path)
+    {
+        $colLignes=Get-Content $file_path
+
+        Foreach ($ligne in $colLignes)
+        {
+            $tabCompte=$ligne.Split("/")
+            create_users($tabCompte)
+        }
+    }
+    else {
+        Write "File $file_path does not exist" >> $logfile
+    }
+}
+
+# Exécution
+lire_fichier($fichier)
