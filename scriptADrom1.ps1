fed7c34316694981d4049d4556453560730658be
knonn <romain.besland@gmail.com>
2015-11-24 09:14:06 +0100

    add logfile function

--- scriptADrom1.ps1
+++ scriptADrom1.ps1
@@ -1,44 +1,6 @@
 $fichier="C:\users.txt"
 $logfile="C:\logfile.log"
-
-#Creation user basic
-function create_users {
-    param([string[]]$params)
-    $local=[ADSI]"WinNT://."
-    $nom=$params[0]
-    $nom_complet=$params[1]
-    $description=$params[2]
-    $compte=[ADSI]"WinNT://./$nom"
-    $date=Get-Date -UFormat "%Y-%m-%d %H:%M"
-    If (!$compte.path)
-    {
-        $utilisateur=$local.create("user",$nom)
-        $utilisateur.InvokeSet("Description",$description)
-        $utilisateur.InvokeSet("FullName",$nom_complet)
-        $utilisateur.CommitChanges()
-        Write "Add $nom : success" >> $logfile
-    }
-    else {
-        Write "[$date] User $nom already exist" >> $logfile
-    }
-}
-
-function lire_fichier {
-    param([string]$file_path)
-    If (Test-Path $file_path)
-    {
-        $colLignes=Get-Content $file_path
-
-        Foreach ($ligne in $colLignes)
-        {
-            $tabCompte=$ligne.Split("/")
-            create_users($tabCompte)
-        }
-    }
-    else {
-        Write "File $file_path does not exist" >> $logfile
-    }
-}
+$date=Get-Date -UFormat "%Y-%m-%d %H:%M"
 
 #Creation d'une OU
 function create_ou {
@@ -55,11 +17,28 @@ function adduser {
     $prenom=$params[1]
     $description=$params[2]
     $ou=$params[3]
-    New-ADUser -name $prenom" "$nom -ChangePasswordAtLogon 1 -Path "ou=$ou,ou=stages,dc=newyork,dc=domain" -Description $description -DisplayName $prenom" "$nom -Enabled $true -GivenName $prenom -SamAccountName $prenom"."$nom -AccountPassword (ConvertTo-SecureString "P@ssword" -AsPlainText -force)
+    If (dsquery user -samid "$prenom.$nom")
+    {
+        Write "[$date] User $nom already exist" >> $logfile
+    }
+    else {
+        New-ADUser -name $prenom" "$nom -ChangePasswordAtLogon 1 -Path "ou=$ou,ou=stages,dc=newyork,dc=domain" -Description $description -DisplayName $prenom" "$nom -Enabled $true -GivenName $prenom -SamAccountName $prenom"."$nom -AccountPassword (ConvertTo-SecureString "P@ssword" -AsPlainText -force)
+        Write "[$date] Add user $prenom.$nom : success" >> $logfile
+    }
     #Add-ADGroupMember -Identity "$ou" -Member $prenom"."$nom
 }
 
-# Ex�cution
-#lire_fichier($fichier)
-#create_ou("test1")
-adduser(@("besland","romain","desc","test1"))
+# Ex�cution
+$nom=Read-Host -Prompt "Utilisateur - Nom :"
+$prenom=Read-Host -Prompt "Utilisateur - Prénom"
+$description=Read-Host -Prompt "Utilisateur - Description"
+$ou=Read-Host -Prompt "Dans quelle unité d'organisation doit figurer l'utilisateur ?"
+If ([ADSI]::Exists("LDAP://OU=$ou,OU=stages,DC=newyork,DC=domain"))
+{
+    Write "[$date] OU $ou already exist" >> $logfile
+}
+else {
+    create_ou($ou)
+    Write "[$date] OU $ou added successfully" >> $logfile
+}
+adduser(@($nom,$prenom,$description,$ou))
